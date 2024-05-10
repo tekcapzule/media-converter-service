57bdd5ecff2f5eb89e0caf50cb4ac565642bc25d
MissionL1 <aimissionl1@gmail.com>
2024-05-10 23:51:16 +0530

    deploy issue

--- src/main/java/com/tekcapzule/mediaconverter/application/function/ConvertMediaFunction.java
+++ src/main/java/com/tekcapzule/mediaconverter/application/function/ConvertMediaFunction.java
@@ -3,6 +3,7 @@ package com.tekcapzule.mediaconverter.application.function;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.tekcapzule.mediaconverter.application.config.AppConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import software.amazon.awssdk.services.mediaconvert.*;
@@ -10,14 +11,20 @@ import software.amazon.awssdk.services.mediaconvert.model.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Component
 @Slf4j
-public class ConvertMediaFunction implements RequestHandler<S3Event, String> {
+public class ConvertMediaFunction implements Function<S3Event, String> {
     private static final String OUTPUT_BUCKET_NAME = "transcodedvideos.tekcapzule.com";
     private static final String OUTPUT_KEY_PREFIX = "transcoded/";
 
-    @Override
+    private final AppConfig appConfig;
+
+    public ConvertMediaFunction( final AppConfig appConfig) {
+        this.appConfig = appConfig;
+    }
+   // @Override
     public String handleRequest(S3Event input, Context context) {
         try {
             // Initialize AWS MediaConvert client
@@ -92,6 +99,49 @@ public class ConvertMediaFunction implements RequestHandler<S3Event, String> {
             } else {
                 throw new RuntimeException("Error in transcoding job: " + jobStatus);
             }
+        }
+    }
+
+    @Override
+    public String apply(S3Event s3Event) {
+        try {
+            // Initialize AWS MediaConvert client
+            MediaConvertClient mediaConvertClient = MediaConvertClient.builder().build();
+
+            String bucketName = s3Event.getRecords().get(0).getS3().getBucket().getName();
+            String objectKey = s3Event.getRecords().get(0).getS3().getObject().getKey();
+            System.out.println("S3Event ObjectKey"+objectKey);
+
+            // Construct the input file URI for MediaConvert job
+            String inputFileUri = "s3://" + bucketName + "/" + objectKey;
+            // Create audio selector with default selection
+            AudioSelector audioSelector =  AudioSelector.builder().defaultSelection(AudioDefaultSelection.DEFAULT).build();
+            Map<String,AudioSelector> audios = new HashMap<>();
+            audios.put("Audio Selector 1",audioSelector);
+
+            // Create a job settings object
+            CreateJobResponse createJobResponse = mediaConvertClient.createJob(CreateJobRequest.builder()
+                    .role("arn:aws:iam::552781593359:role/service-role/MediaConvert_Default_Role")
+                    .settings(JobSettings.builder()
+                            .outputGroups(
+                                    createOutputGroup("MobileGroup", "mobile/", "system-Avc_16x9_480p_29_97fps_800kbps")
+                                    //  createOutputGroup("TabletGroup", "tablet/", "System-Avc_16x9_720p_29_97fps_2000kbps"),
+                                    //createOutputGroup("WebGroup", "web/", "System-Avc_16x9_720p_29_97fps_4000kbps")
+                            )
+                            .inputs(Input.builder().fileInput(inputFileUri).audioSelectors(audios).build())
+                            .build())
+                    .build());
+
+            // Wait for the job to complete
+            waitForJobCompletion(mediaConvertClient, createJobResponse.job().id());
+
+            // Cleanup resources
+            mediaConvertClient.cancelJob(CancelJobRequest.builder().build());
+
+            return "Transcoding job completed successfully.";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error in transcoding job: " + e.getMessage();
         }
     }
 }
